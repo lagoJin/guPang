3a9a03ac242e5e0494877487b737191c6a12b13c
chlee <devstory.co.kr@gmail.com>
2019-04-21 18:23:17 +0900

    add CartActivity

---
+++ app/src/main/java/kr/co/express9/client/mvvm/view/CartActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.express9.client.mvvm.view
+
+import kr.co.express9.client.R
+import kr.co.express9.client.base.BaseActivity
+import kr.co.express9.client.databinding.ActivityCartBinding
+
+class CartActivity :BaseActivity<ActivityCartBinding>(R.layout.activity_cart) {
+
+    override fun initStartView() {
+
+    }
+
+}
