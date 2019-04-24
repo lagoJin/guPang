cd1998928260836d87733433033f86222e202bf1
rkd6187@naver.com <wlsgh112>
2019-04-24 23:45:45 +0900

    map scroll 추가

--- app/src/main/java/kr/co/express9/client/mvvm/view/fragment/MapFragment.kt
+++ app/src/main/java/kr/co/express9/client/mvvm/view/fragment/MapFragment.kt
@@ -9,12 +9,10 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.text.Editable
-import android.view.View
+import android.util.TypedValue
 import androidx.annotation.RequiresPermission
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
-import androidx.viewpager.widget.ViewPager
-import androidx.viewpager2.widget.ViewPager2
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -23,8 +21,10 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.jakewharton.rxbinding3.widget.textChanges
 import com.tedpark.tedpermission.rx2.TedRx2Permission
+import com.yarolegovich.discretescrollview.InfiniteScrollAdapter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kr.co.express9.client.R
+import kr.co.express9.client.adapter.ItemTransformer
 import kr.co.express9.client.adapter.MapMarketAdapter
 import kr.co.express9.client.base.BaseFragment
 import kr.co.express9.client.databinding.FragmentMapBinding
@@ -70,10 +70,10 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
         })
 
         disposable.add(dataBinding.actvMapSearch.textChanges()
-                .filter { it.isNotEmpty() }
-                .debounce(300, TimeUnit.MILLISECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { kakaoAddressViewModel.getAddressList(it as Editable) })
+            .filter { it.isNotEmpty() }
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { kakaoAddressViewModel.getAddressList(it as Editable) })
 
         dataBinding.ivMapLocation.setOnClickListener {
             if (::map.isInitialized && ::location.isInitialized) {
@@ -84,19 +84,22 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
         initAdapter()
     }
 
-    fun initAdapter() {
+    private fun initAdapter() {
         val marketAdapter = MapMarketAdapter()
-        dataBinding.vpMap.adapter = marketAdapter
+        val infiniteScrollAdapter = InfiniteScrollAdapter.wrap(marketAdapter)
+        dataBinding.vpMap.adapter = infiniteScrollAdapter
         dataBinding.vpMap.apply {
-            setPadding(100, 0, 100, 0)
+            dataBinding.vpMap.setItemTransformer(ItemTransformer())
+            val space = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics)
+            //addItemDecoration(ItemTransformer(space))
         }
-        dataBinding.vpMap.setPageTransformer { page, position ->
+        /*dataBinding.vpMap.setPageTransformer { page, position ->
             when {
                 dataBinding.vpMap.currentItem == 0 -> page.translationX = (-20f)
                 dataBinding.vpMap.currentItem == marketAdapter.itemCount -> page.translationX = 20f
                 else -> page.translationX = 0f
             }
-        }
+        }*/
     }
 
     override fun onMapReady(map: GoogleMap?) {
@@ -117,27 +120,27 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
         mapFragment.getMapAsync(this)
 
         if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
-                        activity!!,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                )
-                && PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
-                        activity!!,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                )
+                activity!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            && PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
+                activity!!,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
         ) {
             TedRx2Permission.with(activity!!)
-                    .setRationaleTitle(R.string.permission_title)
-                    .setRationaleMessage(R.string.permission_content) // "we need permission for read contact and find your location"
-                    .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
-                    .request()
-                    .subscribe({ tedPermissionResult ->
-                        if (tedPermissionResult.isGranted) {
-                            initLocation()
-                            activity!!.toast("Permission Granted")
-                        } else {
-                            activity!!.toast("Permission Denied\n" + tedPermissionResult.getDeniedPermissions().toString())
-                        }
-                    }, { throwable -> })
+                .setRationaleTitle(R.string.permission_title)
+                .setRationaleMessage(R.string.permission_content) // "we need permission for read contact and find your location"
+                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .request()
+                .subscribe({ tedPermissionResult ->
+                    if (tedPermissionResult.isGranted) {
+                        initLocation()
+                        activity!!.toast("Permission Granted")
+                    } else {
+                        activity!!.toast("Permission Denied\n" + tedPermissionResult.getDeniedPermissions().toString())
+                    }
+                }, { throwable -> })
             return
         } else {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3000, 10f, locationListener)
