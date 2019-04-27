2b4427fd8fbe385e79fa1b4da043cdecac9482a2
chlee <devstory.co.kr@gmail.com>
2019-04-27 23:07:00 +0900

    상품 상세 페이지 레이아웃 작업

--- app/src/main/java/kr/co/express9/client/mvvm/view/GoodsActivity.kt
+++ app/src/main/java/kr/co/express9/client/mvvm/view/GoodsActivity.kt
@@ -1,15 +1,36 @@
 package kr.co.express9.client.mvvm.view
 
+import android.view.Menu
+import android.view.MenuItem
 import kr.co.express9.client.R
 import kr.co.express9.client.base.BaseActivity
 import kr.co.express9.client.databinding.ActivityGoodsBinding
 import kr.co.express9.client.mvvm.viewModel.CategoryGoodsViewModel
+import kr.co.express9.client.util.extension.launchActivity
 
 class GoodsActivity : BaseActivity<ActivityGoodsBinding>(R.layout.activity_goods) {
 
     override fun initStartView() {
-        val goods = intent.getSerializableExtra("goods") as CategoryGoodsViewModel.GoodsDummy
-        dataBinding.tvGoodsName.text = goods.name
+        dataBinding.goods = intent.getSerializableExtra("goods") as CategoryGoodsViewModel.GoodsDummy
+
+        // action bar 등록
+        setSupportActionBar(dataBinding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.cart -> launchActivity<CartActivity>()
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_cart, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 
 }
