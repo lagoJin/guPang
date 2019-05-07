95a4bbc47d1ca7870e7a86eb671bf8c58e52c35f
chlee <devstory.co.kr@gmail.com>
2019-05-07 20:27:30 +0900

    firebase cloud messaging 기존 작업 소스로 합침

--- app/src/main/java/kr/co/express9/client/thirdParty/firebase/CloudMessagingService.kt
+++ app/src/main/java/kr/co/express9/client/thirdParty/firebase/CloudMessagingService.kt
@@ -1,33 +1,59 @@
 package kr.co.express9.client.thirdParty.firebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kr.co.express9.client.R
 import kr.co.express9.client.util.Logger
 
 class CloudMessagingService : FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Logger.d("From: ${remoteMessage.from}")
-        if (remoteMessage.data.isNotEmpty()) {
-            Logger.d("Message data payload ${remoteMessage.data}")
-
-//            if (/* Check if data needs to be processed by long running job */ true) {
-//                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-//                scheduleJob()
-//            } else {
-//                // Handle message within 10 seconds
-//                handleNow()
-//            }
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        Logger.d("From: ${remoteMessage?.from}")
+        remoteMessage?.data?.isNotEmpty()?.let {
+            Logger.d("Message data payload:" + remoteMessage.data)
         }
 
         // Check if message contains a notification payload.
-        if (remoteMessage.notification != null) {
-            Logger.d("Message Notification Body: ${remoteMessage.notification!!.body}")
+        remoteMessage?.notification?.let {
+            Logger.d("Message Notification Body: ${it.body}")
+            sendNotification(it)
         }
     }
 
     override fun onNewToken(token: String) {
         Logger.d("Refreshed token: $token")
         // sent registration to server
+    }
+
+    private fun sendNotification(remoteMessageNotification: RemoteMessage.Notification) {
+
+        val channelId = getString(R.string.notification_channel_id)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(remoteMessageNotification.title)
+                .setContentText(remoteMessageNotification.body)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelName = getString(R.string.app_name)
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 }
