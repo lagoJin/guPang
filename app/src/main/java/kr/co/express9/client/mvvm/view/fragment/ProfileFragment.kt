822a5e1f9d1fdaac78e9bfc20870b6a2ac7adbd4
chlee <devstory.co.kr@gmail.com>
2019-05-08 21:36:25 +0900

    마이페이지 유저 이미지, 이름 추가

--- app/src/main/java/kr/co/express9/client/mvvm/view/fragment/ProfileFragment.kt
+++ app/src/main/java/kr/co/express9/client/mvvm/view/fragment/ProfileFragment.kt
@@ -1,12 +1,22 @@
 package kr.co.express9.client.mvvm.view.fragment
 
+import com.bumptech.glide.Glide
 import kr.co.express9.client.R
 import kr.co.express9.client.base.BaseFragment
 import kr.co.express9.client.databinding.FragmentProfileBinding
+import kr.co.express9.client.mvvm.model.data.User
+import kr.co.express9.client.util.setImageUrl
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
 
     override fun initStartView(isRestart: Boolean) {
-
+        val user = User.getUser()
+        dataBinding.tvName.text = user.name
+        user.image?.let {
+            Glide.with(this)
+                .load(it)
+                .error(R.drawable.ic_account_64_px)
+                .into(dataBinding.ivProfile)
+        }
     }
 }
