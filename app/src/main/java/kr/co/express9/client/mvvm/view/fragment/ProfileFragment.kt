6db2cf1fa9b666960a9e26cdfaf46f9d11eb3c0e
chlee <devstory.co.kr@gmail.com>
2019-05-08 23:06:57 +0900

    1:1 문의 이메일 연동

--- app/src/main/java/kr/co/express9/client/mvvm/view/fragment/ProfileFragment.kt
+++ app/src/main/java/kr/co/express9/client/mvvm/view/fragment/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package kr.co.express9.client.mvvm.view.fragment
 
+import android.content.Intent
 import com.bumptech.glide.Glide
 import kr.co.express9.client.R
 import kr.co.express9.client.base.BaseFragment
@@ -7,7 +8,7 @@ import kr.co.express9.client.databinding.FragmentProfileBinding
 import kr.co.express9.client.mvvm.model.data.User
 import kr.co.express9.client.mvvm.view.NotificationSettingActivity
 import kr.co.express9.client.util.extension.launchActivity
-import kr.co.express9.client.util.setImageUrl
+
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
 
@@ -18,14 +19,35 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_p
         dataBinding.llNotificationSetting.setOnClickListener {
             activity?.launchActivity<NotificationSettingActivity>()
         }
-        dataBinding.llInquire.setOnClickListener {
-
-        }
+        dataBinding.llInquire.setOnClickListener { sendEmail() }
         user.image?.let {
             Glide.with(this)
                 .load(it)
                 .error(R.drawable.ic_account_64_px)
                 .into(dataBinding.ivProfile)
+        }
+    }
+
+    private fun sendEmail() {
+        val emailIntent = Intent(Intent.ACTION_SEND)
+        val csvEmail = getString(R.string.csv_eamil)
+
+        try {
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(csvEmail))
+
+            emailIntent.type = "text/html"
+            emailIntent.setPackage("com.google.android.gm")
+            if (emailIntent.resolveActivity(activity!!.packageManager) != null)
+                startActivity(emailIntent)
+
+            startActivity(emailIntent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+
+            emailIntent.type = "text/html"
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(csvEmail))
+
+            startActivity(Intent.createChooser(emailIntent, "Send Email"))
         }
     }
 }
